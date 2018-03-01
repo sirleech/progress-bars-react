5bc64a8ee5dc6edd8dcf3ebfb6b8274518bdb285
Chris Lee <sirleech@gmail.com>
2018-03-02 10:01:48 +1100

    static progress numerals and title

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p><progress max="100" value="80"></progress></p>
-        <p><progress max="100" value="33"></progress></p>
-        <p><progress max="100" value="33"></progress></p>
-        <p><progress max="100" value="44"></progress></p>
+        <p>00%<progress max="100" value="80"></progress></p>
+        <p>00%<progress max="100" value="33"></progress></p>
+        <p>00%<progress max="100" value="33"></progress></p>
+        <p>00%<progress max="100" value="44"></progress></p>
 
         <select id="bar-select">
           <option value="bar1">Bar 1</option>
