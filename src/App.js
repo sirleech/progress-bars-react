53f0e0af91d5984f23716dc3515174af0c08c421
Chris Lee <sirleech@gmail.com>
2018-03-02 10:07:35 +1100

    style buttons

--- src/App.js
+++ src/App.js
@@ -11,16 +11,22 @@ class App extends Component {
         <p>00%<progress max="100" value="33"></progress></p>
         <p>00%<progress max="100" value="44"></progress></p>
 
-        <select id="bar-select">
-          <option value="bar1">Bar 1</option>
-          <option value="bar2">Bar 2</option>
-          <option value="bar3">Bar 3</option>
-          <option value="bar4">Bar 4</option>
-        </select>
+        <p>
+          <label>Select your bar</label>
+          <select id="bar-select">
+            <option value="bar1">Bar 1</option>
+            <option value="bar2">Bar 2</option>
+            <option value="bar3">Bar 3</option>
+            <option value="bar4">Bar 4</option>
+          </select>
+        </p>
 
-        <button>0%</button>
-        <button>0%</button>
-        <button>0%</button>
+        <p>
+          <label>Change progress</label>
+          <button>0%</button>
+          <button>0%</button>
+          <button>0%</button>
+        </p>
 
       </div>
     );
